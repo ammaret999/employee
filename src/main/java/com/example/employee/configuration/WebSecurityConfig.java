86082cf51d5df70403ccc3d9766eeba4bdaea63c
kao <ammaret.a@appman.co.th>
2023-03-15 15:49:17 +0700

    SecurityFilterChain-login

--- src/main/java/com/example/employee/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/employee/configuration/WebSecurityConfig.java
@@ -1,29 +1,47 @@
 package com.example.employee.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
-//                .requestMatchers(HttpMethod.POST, "/employee").permitAll()
-                .anyRequest().authenticated()
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .requestMatchers("/")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .oauth2Login()
-                .defaultSuccessUrl("/success")
+                .httpBasic()
                 .and()
-                .build();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
     }
+
+
+//    @Bean
+//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests()
+//                .requestMatchers("/**").permitAll()
+////                .requestMatchers(HttpMethod.POST, "/employee").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login()
+//                .defaultSuccessUrl("/success")
+//                .and()
+//                .build();
+//    }
 
 }
