eba53404e152c3d83d3f2b694693f7c34091d5c7
kao <ammaret.a@appman.co.th>
2023-05-11 18:39:52 +0700

    commit-to-P-big-BE

--- src/main/java/com/example/employee/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/employee/configuration/SecurityConfiguration.java
@@ -21,6 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/token").permitAll()
                 .antMatchers("**").fullyAuthenticated()
                 .anyRequest().authenticated()
                 .and()
