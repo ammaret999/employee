007a54636eab79be4c314b6b7fbffb9b1d2d3343
kao <ammaret.a@appman.co.th>
2023-06-12 17:05:31 +0700

    unit-test-h2-mock

--- src/test/java/com/example/employee/EmployeeApplicationTests.java
+++ src/test/java/com/example/employee/EmployeeApplicationTests.java
@@ -1,9 +1,40 @@
 package com.example.employee;
 
+import com.example.employee.dtoIn.TitleNameDTO;
+import com.example.employee.model.TitleNameModel;
+import com.example.employee.repository.TitleNameRepository;
+import com.example.employee.service.TitleNameService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EmployeeApplicationTests {
+    @Autowired
+    private TitleNameService titleNameService;
+    @Autowired
+    private TitleNameRepository titleNameRepository;
+
+    @Test
+    @Transactional
+    public void testSaveEmployee() {
+        TitleNameDTO titleNameDTO = new TitleNameDTO();
+        titleNameDTO.setTitleName("Sample Title");
+
+        TitleNameModel savedTitleName = titleNameService.createTitleName(titleNameDTO);
+
+        TitleNameModel retrievedTitleName = titleNameRepository.findById(savedTitleName.getId()).orElse(null);
+
+        assertNotNull(retrievedTitleName);
+        assertEquals(titleNameDTO.getTitleName(), retrievedTitleName.getTitleName());
+    }
 
 }
